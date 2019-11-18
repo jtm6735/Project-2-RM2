d29ec9a695a3d103350eb401b73fadf7d73ada15
jtm6735 <jtm6735@rit.edu>
2019-11-18 08:34:53 -0500

    more length fixes

--- server/router.js
+++ server/router.js
@@ -5,7 +5,6 @@ const router = (app) => {
   app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
   app.get('/getQuizzes', mid.requiresSecure, controllers.Quiz.getQuizzes);
   app.get('/quizPage', mid.requiresSecure, controllers.Account.quizPage);
-  //app.get('/upgradePage', mid.requiresSecure, mid.requiresLogin, )
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
@@ -14,7 +13,8 @@ const router = (app) => {
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
   app.get('/maker', mid.requiresLogin, controllers.Quiz.makerPage);
   app.post('/maker', mid.requiresLogin, controllers.Quiz.make);
-  app.post('/changePassword', mid.requiresSecure, mid.requiresLogin, controllers.Account.changePassword);
+  app.post('/changePassword', mid.requiresSecure, mid.requiresLogin, 
+           controllers.Account.changePassword);
   app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.get('/*', mid.requiresSecure, mid.requiresLogin, controllers.Account.notFoundPage);
 };
